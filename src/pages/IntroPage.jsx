6f2f583c57ba4c58d3b5735a45ff5c33b30d174b
pierrecw <leepierre456@gmail.com>
2022-08-10 01:15:26 +0900

    [UI] IntroPage UI 구성
    [UI] IntroPage UI 구성
    Logo, Button, BackGround Component로 구성
    일부 값 상수화 필요성

--- src/pages/IntroPage.jsx
+++ src/pages/IntroPage.jsx
@@ -1,8 +1,15 @@
+import Logo from "../components/Composition/Logo";
+import BackGroundCover from "../components/ui/BackGroundCover";
+import Button from "../components/ui/Button";
 
-export default function MainPage(){
+export default function IntroPage(){
     return (
-        <>
-        <div>Intro Page</div>
-        </>
+        <div>
+            <Logo/>
+            <Button>
+                시작하기
+            </Button>
+            <BackGroundCover backgroundColor='#FC0'/>
+        </div>
     )
 }
