40f2dcbf8097db2d6f53bc2f8eb9e7c2e3cffdcb
pierrecw <leepierre456@gmail.com>
2022-08-10 04:42:23 +0900

    [ADD] Input 컴포넌트 배열
    [ADD] Input 컴포넌트 배열

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,5 +1,16 @@
+import GeneralHeader from "../components/Composition/GeneralHeader";
+import Input from '../components/ui/Input'
 export default function RegisterPage() {
   return (
-    <div>RegisterPage</div>
+    <div>
+        <GeneralHeader title="신상 정보 기입 페이지"/>
+        <div style={{
+            margin : '0 20px'}
+            }>
+            <Input/>
+            <Input/>
+            <Input/>
+        </div>
+    </div>
   )
 }
