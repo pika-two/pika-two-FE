7e6508558bd92d412287b6826a494d9d9fbb0d9d
pierrecw <leepierre456@gmail.com>
2022-08-09 15:50:50 +0900

    [RESTORE] App 초기화
    [RESTORE] App 초기화 및 Layout 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import { css } from 'styled-components';
 import styled from 'styled-components';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <Wrapper>
-        <Title>
-          Hello World!
-        </Title>
-      </Wrapper>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+  <Layout>
+    {<div>안녕하세요</div>}
+  </Layout>
   )
 }
 
-
-// Create a Title component that'll render an <h1> tag with some styles
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
-
-// Create a Wrapper component that'll render a <section> tag with some styles
-const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
-`;
 export default App
+
+const Layout = styled.div`
+max-width: 100%;
+width: 100%;
+padding: 0;
+margin: 0;
+height: auto;
+background-color : blue;
+`
