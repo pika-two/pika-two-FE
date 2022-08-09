837fa80d741396e3419a8732bdd7522f7845b103
pierrecw <leepierre456@gmail.com>
2022-08-10 04:26:31 +0900

    [ADD] Top02 컴포넌트 추가
    [ADD] Top02 컴포넌트 추가

---
+++ src/components/ui/Top/Top02.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Top02 = styled.h3`
+    white-space: pre-line;
+    font-weight: bold;
+    padding: 0 24px;
+    font-size: 32px;
+    line-height: 40px;
+    word-break: keep-all;
+    text-align : center;
+`
+export default Top02
