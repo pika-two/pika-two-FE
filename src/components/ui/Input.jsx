53ce7157fe85afc5b934a816694f12f8aeb9ea9d
pierrecw <leepierre456@gmail.com>
2022-08-10 04:42:05 +0900

    [ADD] Input 컴포넌트 추가
    [ADD] Input 컴포넌트 추가

---
+++ src/components/ui/Input.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const InputStyled = styled.input`
+width : 100%;
+height : 48px;
+border:2px solid #aaa;
+border-radius:4px;
+margin:8px 0;
+outline:none;
+padding:8px;
+box-sizing:border-box;
+transition:.3s;
+background-color : #f6f6f6;
+:focus{
+    border-color:dodgerBlue;
+    box-shadow:0 0 8px 0 dodgerBlue;
+}`
+
+export default function Input({...rest}) {
+  return (
+    <InputStyled {...rest}/>
+  )
+}
