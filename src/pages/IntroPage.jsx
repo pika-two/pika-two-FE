ff009fbfc4e06f7dcbac89d3fbaf13a87d71a645
pierrecw <leepierre456@gmail.com>
2022-08-10 01:19:57 +0900

    [FEAT] 가입페이지 이동 네비게이션 제공
    [FEAT] 가입페이지 이동 네비게이션 제공

--- src/pages/IntroPage.jsx
+++ src/pages/IntroPage.jsx
@@ -1,12 +1,13 @@
 import Logo from "../components/Composition/Logo";
 import BackGroundCover from "../components/ui/BackGroundCover";
 import Button from "../components/ui/Button";
-
+import { useInternalRouter } from "./routing";
 export default function IntroPage(){
+    const {push} = useInternalRouter();
     return (
         <div>
             <Logo/>
-            <Button>
+            <Button onClick={()=>push('/registerPage')}>
                 시작하기
             </Button>
             <BackGroundCover backgroundColor='#FC0'/>
