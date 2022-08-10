bbf8e55159d21429288efc990e1cd45fcfe4a54c
Seoyoung <seoyoung9346@naver.com>
2022-08-10 11:00:51 +0900

    서명 텍스트 컴포넌트 추가

---
+++ src/components/ui/message.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const message = styled.p`
+    white-space: pre-line;
+    padding: 0 10px;
+    font-size: 20px;
+    line-height: 40px;
+    word-break: keep-all;
+    text-align : center;
+    color: #000;
+    margin: 2%;
+    font-family: "Noto Sans KR", sans-serif;
+`;
+export default message;
