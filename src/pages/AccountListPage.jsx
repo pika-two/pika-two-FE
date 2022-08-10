80a65ada1c55bed9c91c69f47f6dfc3f40fc3ae1
pierrecw <leepierre456@gmail.com>
2022-08-10 10:26:24 +0900

    [ADD] incomeList 페이지로 이동 추가
    [ADD] incomeList 페이지로 이동 추가
    
    불필요한 코드 삭제

--- src/pages/AccountListPage.jsx
+++ src/pages/AccountListPage.jsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 import GeneralHeader from "../components/Composition/GeneralHeader";
 import FixedBottomButton from "../components/ui/FixedBottomButton";
 import AccountsList from "../components/Composition/AccountsList";
+import { useInternalRouter } from "./routing";
 const mockDataKeyList = ['account_id','account_name','account_number','bank_name','amount']
 
 export default function AccountListPage() {
-
+  const {push} = useInternalRouter();
   const [accountList, setAccountList] = useState([]);
   useEffect(()=>{
     setAccountList(()=>{
@@ -30,27 +31,7 @@ export default function AccountListPage() {
         <div>
             <AccountsList onClick={()=>{alert('안녕')}} accounts={accountList}></AccountsList>
         </div>
-        <FixedBottomButton background="#FFCC00" color="white">선택</FixedBottomButton>
+        <FixedBottomButton onClick = {()=>{push('/incomeList')}} background="#FFCC00" color="white">선택</FixedBottomButton>
     </div>
   )
-}
-
-const Item = function(){
-    return (
-        <div style={{
-            display : "flex",
-            padding : "10px 0px"
-        }}>
-    
-            <img width={80} height = {80} src="https://play-lh.googleusercontent.com/K1rBzVJjq9qtSxC2vkWIsp3GPtPOCwLPf1aCJ0tL0TvWdNcAKQKhYwjPMY90XJwyW-E" alt=""/>
-            <div style={{
-                display : "flex",
-                flexDirection : "column",
-            }}>
-                <div>KB 국민 ONE통장</div>
-                <div>계좌번호 잔뜩</div>
-                <div>금액 잔뜩</div>
-            </div>
-        </div>
-    )
 }
