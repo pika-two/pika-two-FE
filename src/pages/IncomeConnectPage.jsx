4d40c2518f1408cb49d08b757ab188de42dde2d2
pierrecw <leepierre456@gmail.com>
2022-08-10 15:10:28 +0900

    [ADD] IncomeConnectPage 추가
    [ADD] IncomeConnectPage 추가

---
+++ src/pages/IncomeConnectPage.jsx
@@ -0,0 +1,11 @@
+import BackGroundCover from "../components/ui/BackGroundCover"
+
+
+export default function IncomeConnectPage() {
+  return (
+    <div>
+
+        <BackGroundCover backgroundColor='#FC0'/>
+    </div>
+  )
+}
