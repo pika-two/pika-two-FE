80f8539e36622cc0d8021e0c30e3a080eedf7232
Seoyoung <seoyoung9346@naver.com>
2022-08-10 15:21:33 +0900

    급여내역 선택 페이지 디자인 수정

--- src/components/ui/message.jsx
+++ src/components/ui/message.jsx
@@ -5,11 +5,11 @@ const Message = styled.p`
     white-space: pre-line;
     padding: 0 10px;
     font-size: 19px;
-    line-height: 40px;
+    line-height: 30px;
     word-break: keep-all;
     text-align : center;
     color: #000;
-    margin: 2%;
+    margin: 20px 0;
     font-family: "Noto Sans KR", sans-serif;
 `;
 
