3a2ef6e9080d31e7c6b50630527b73a27a694768
pierrecw <leepierre456@gmail.com>
2022-08-10 16:00:24 +0900

    [ADD] MainPage에 Input 컴포넌트 추가
    [ADD] MainPage에 Input 컴포넌트 추가

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,22 @@
 import RightOnlyHeader from "../components/Composition/RightOnlyHeader"
+import Blank from "../components/ui/Blank";
 import MyPageIcon from '../components/ui/icon/MyPageIcon'
+import Input from '../components/ui/Input'
 import { useInternalRouter } from "./routing";
 export default function MainPage() {
   const {push} = useInternalRouter();
   return (
     <div>
       <RightOnlyHeader right={<MyPageIcon onClick={()=>push('/myPage')}/>} title="KB 돌아온 구직자"></RightOnlyHeader>
+      <Blank/>
+      <Blank/>
+      <div style={ {
+        margin : "0 5vw"
+        }}>
+        <Input />
 
+      </div>
+        
     </div>
   )
 }
