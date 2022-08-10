21a8e87a1e0df33fda520aef7c435738fb126cba
Seoyoung <seoyoung9346@naver.com>
2022-08-10 11:14:21 +0900

    설명  텍스트 컴포넌트 추가

--- src/components/ui/message.jsx
+++ src/components/ui/message.jsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
 const Message = styled.p`
+
     white-space: pre-line;
     font-weight: bold;
     padding: 0 10px;
-    font-size: 20px;
+    font-size: 19px;
     line-height: 40px;
     word-break: keep-all;
     text-align : center;
@@ -12,4 +13,6 @@ const Message = styled.p`
     margin: 2%;
     font-family: "Noto Sans KR", sans-serif;
 `;
+
+
 export default Message;
