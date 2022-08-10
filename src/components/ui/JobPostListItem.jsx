df6342adb3e35fd528029b8b2d6ffdb94dcc09da
pierrecw <leepierre456@gmail.com>
2022-08-10 17:22:40 +0900

    [ADD] JobPostList 컴포넌트 추가
    [ADD] JobPostList 컴포넌트 추가

---
+++ src/components/ui/JobPostListItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function JobPostListItem() {
+  return (
+    <div style={{
+        display : "flex",
+        flexDirection : "column",
+        margin : "0 10px"
+    }}><img width={80} height={80} src="https://play-lh.googleusercontent.com/K1rBzVJjq9qtSxC2vkWIsp3GPtPOCwLPf1aCJ0tL0TvWdNcAKQKhYwjPMY90XJwyW-E"></img>
+    <div>공고</div>
+    <div>날짜짜~날짜자</div>
+    </div>
+  )
+}
