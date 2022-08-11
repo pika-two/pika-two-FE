315a1b28807c8d555186717ce8f95350eaee6d13
pierrecw <leepierre456@gmail.com>
2022-08-11 13:32:59 +0900

    [ADD] Header 추가
    [ADD] Header 추가

--- src/pages/CompanyInfoPage.jsx
+++ src/pages/CompanyInfoPage.jsx
@@ -1,5 +1,14 @@
+import BothHeader from "../components/Composition/BothHeader"
+import { useInternalRouter } from "./routing"
+import BackIcon from '../components/ui/icon/BackIcon'
+import MyPageIcon from '../components/ui/icon/MyPageIcon'
+
 export default function CompanyInfoPage() {
+  const {goBack, push} = useInternalRouter();
   return (
-    <div>CompanyInfoPage</div>
+    <div>
+        <BothHeader left={<BackIcon onClick={()=>goBack()}/>}  right={<MyPageIcon onClick={()=>push('/myPage')}  />}  title="기업정보"></BothHeader>
+
+    </div>
   )
 }
