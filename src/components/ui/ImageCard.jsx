a72977f7a0f4e9794fea2764c1ea465f31283acb
pierrecw <leepierre456@gmail.com>
2022-08-11 10:39:35 +0900

    [ADD] ImageCard 컴포넌트 추가
    [ADD] ImageCard 컴포넌트 추가

---
+++ src/components/ui/ImageCard.jsx
@@ -0,0 +1,14 @@
+export default function ImageCard({imgae,content,right}) {
+  return (
+    <div style={{
+        display : 'grid',
+        gridTemplateColumns : '60px auto 32px'
+    }}>
+        <div>
+            {imgae?<img src={image} alt=""></img>:<img width={60} height={60} style={{background : '#e0e0e0'}}></img>}
+        </div>
+        {content}
+        {right}
+    </div>
+  )
+}
