f6a9d423989454b76a6f4f961a3391988de477d7
pierrecw <leepierre456@gmail.com>
2022-08-12 10:11:55 +0900

    [ADD] TODO added
    [ADD] TODO added

--- src/pages/CompanyInfoPage.jsx
+++ src/pages/CompanyInfoPage.jsx
@@ -15,16 +15,19 @@ export default function CompanyInfoPage() {
         <Top02>기업이름</Top02>
         <Top02>공고</Top02>
         <p style={{border : '1px solid'}}></p>
+        {/* //TODO middle : 공고에 찜하는 버튼 추가 */}
         <CompanyListItem/>
         <br></br>
 
         <Top02>연봉</Top02>
+        {/* //TODO high : 챠트 라이브러리 찾고 적용 */}
+        {/* //TODO high : 챠트 만들어진 후 챠드 막대 클릭시 연봉정보(SalaryList) 페이지로 이동 */}
         <div style={{
         width : '80vw',
         height : '80vw',
         margin : '0 auto',
         backgroundColor : '#FFC'}}>챠트 들어갈 곳</div>
-
+        {/* //TODO low : axios로 해당 기업리뷰 가져오기 */}
         <Top02> 리뷰 </Top02>
         <ReviewList/>
     </div>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -16,14 +16,17 @@ export default function MainPage() {
           justifyContent: "center",
           background: "#FFCC00",
         }}>
+          {/* //TODO high : 검색을 하기 위한 input 연동 및 enter 이벤트 및 돋보기(클릭 이미지)추가 필요 */}
           <Input/>
         </div>
       <div style={ {
         margin : "10px 5vw"
         }}>
-        
+        {/* //TODO middle : Badge를 클릭했을시 해당 항목 검색 기능 제공 */}
         <BadgeBox/>
         <Blank/>
+        {/* //TODO low : 메인페이지 들어올시 mounted 전에 공고 리스트 불러오기 */}
+        {/* //TODO low : 공고 클릭시 해당회사 상세 페이지 이동 기능 구현 company_id와 연계*/}
         <Top02 color="#000">채용 진행 중인 공고</Top02>
         <JobPostList></JobPostList>
       </div>
