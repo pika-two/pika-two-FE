8666fa6087478020486ae937ce6f833830603088
pierrecw <leepierre456@gmail.com>
2022-08-12 15:42:22 +0900

    [ADD] msw 초기 설정 완료
    [ADD] msw 초기 설정 완료

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import {BrowserRouter} from 'react-router-dom'
+import { worker } from './mocks/browser'
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start()
+}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
