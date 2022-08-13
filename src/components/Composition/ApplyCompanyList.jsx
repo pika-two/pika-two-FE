ae6c01ba81fc5713a08cf67880ea5a871d325feb
pierrecw <leepierre456@gmail.com>
2022-08-14 07:00:36 +0900

    [ADD] 이동 이벤트 추가
    [ADD] 이동 이벤트 추가

--- src/components/Composition/ApplyCompanyList.jsx
+++ src/components/Composition/ApplyCompanyList.jsx
@@ -1,13 +1,15 @@
 import TrashCanIcon from '../ui/icon/TrachCanIcon'
 import ApplyCompanyListItem from '../ui/ApplyCompanyListItem'
+import {useInternalRouter} from '../../pages/routing';
 export default function ApplyCompanyList({applyCompanys, handleDeleteApplyCompany}) {
+  const {push} = useInternalRouter();
   return (
     <div style={{
         display : 'grid',
         gridTemplateColumns : 'auto',
         gap : '1em'
       }}>
-          {applyCompanys.map((applyCompany)=><ApplyCompanyListItem key={applyCompany.apply_id} applyCompanpy={applyCompany} right={<TrashCanIcon onClick={(e)=>handleDeleteApplyCompany(e,applyCompany.apply_id)}/>}/>)}
+          {applyCompanys.map((applyCompany)=><ApplyCompanyListItem key={applyCompany.apply_id}  onClick={()=> {push(`/company/${applyCompany.company_id}`)}}  applyCompanpy={applyCompany} right={<TrashCanIcon onClick={(e)=>handleDeleteApplyCompany(e,applyCompany.apply_id)}/>}/>)}
       </div>
   )
 }
--- src/components/ui/ApplyCompanyListItem.jsx
+++ src/components/ui/ApplyCompanyListItem.jsx
@@ -1,13 +1,13 @@
 import ImageCardBoth from "./ImageCardBoth"
 import Bold from "./Bold"
 
-export default function ApplyCompanyListItem({applyCompanpy,image,right}) {
+export default function ApplyCompanyListItem({applyCompanpy,image,right,onClick}) {
   return (
-    <ImageCardBoth image={image} content={<CompnyInfo applyCompanpy={applyCompanpy}/>} right={right}/>
+    <ImageCardBoth image={image} content={<CompnyInfo onClick={onClick} applyCompanpy={applyCompanpy}/>} right={right}/>
   )
 }
 
-const CompnyInfo = function({applyCompanpy}){
+const CompnyInfo = function({applyCompanpy,onClick}){
     return (
         <div
         style={{
@@ -16,7 +16,9 @@ const CompnyInfo = function({applyCompanpy}){
             justifyContent : "center",
             backgroundColor : "#F6F6F6",
             margin : "0 0 5px 10px",
-        }}>
+        }}
+        onClick={onClick}
+        >
             <Bold>{applyCompanpy.company_name}  </Bold>
             <div style = {{fontFamily : "two", textAlign : "center", margin: " 0, vh"}}>{applyCompanpy.post_title}{applyCompanpy.status}<br></br>
             {applyCompanpy.start_dt}- {applyCompanpy.end_dt}</div>
