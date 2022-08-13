39740300dfed69535cbbda050de71e195bee7ef8
pierrecw <leepierre456@gmail.com>
2022-08-14 06:56:02 +0900

    [ADD] 데이터 바인딩 및 삭제 이벤트 추가
    [ADD] 데이터 바인딩 및 삭제 이벤트 추가

--- src/components/Composition/ApplyCompanyList.jsx
+++ src/components/Composition/ApplyCompanyList.jsx
@@ -1,18 +1,13 @@
 import TrashCanIcon from '../ui/icon/TrachCanIcon'
 import ApplyCompanyListItem from '../ui/ApplyCompanyListItem'
-export default function ApplyCompanyList() {
+export default function ApplyCompanyList({applyCompanys, handleDeleteApplyCompany}) {
   return (
     <div style={{
         display : 'grid',
         gridTemplateColumns : 'auto',
         gap : '1em'
       }}>
-          <ApplyCompanyListItem right={<TrashCanIcon/>}/>
-          <ApplyCompanyListItem right={<TrashCanIcon/>}/>
-          <ApplyCompanyListItem right={<TrashCanIcon/>}/>
-          <ApplyCompanyListItem right={<TrashCanIcon/>}/>
-          <ApplyCompanyListItem right={<TrashCanIcon/>}/>
-          <ApplyCompanyListItem right={<TrashCanIcon/>}/>
+          {applyCompanys.map((applyCompany)=><ApplyCompanyListItem key={applyCompany.apply_id} applyCompanpy={applyCompany} right={<TrashCanIcon onClick={(e)=>handleDeleteApplyCompany(e,applyCompany.apply_id)}/>}/>)}
       </div>
   )
 }
--- src/pages/ApplyCompanyListPage.jsx
+++ src/pages/ApplyCompanyListPage.jsx
@@ -3,18 +3,71 @@ import MyPageIcon from '../components/ui/icon/MyPageIcon'
 import BackIcon from "../components/ui/icon/BackIcon";
 import { useInternalRouter } from "./routing"
 import ApplyCompanyList from '../components/Composition/ApplyCompanyList'
+import { useState } from "react";
+
 export default function ApplyCompanyListPage() {
+  const dummyData = [{
+    'apply_id': 1,
+    'post_id': 0,
+     'post_title': '2022년 상반기 공채 1',
+     'start_dt' : '0000-01-01',
+     'end_dt' : '9999-12-31',
+     'type': '공채',
+     'company_id': 0,
+     'company_name': '국민은행',
+     'status': '진행중',
+   },
+   {
+    'apply_id': 2,
+    'post_id': 0,
+     'post_title': '2022년 상반기 공채 2',
+     'start_dt' : '0000-01-01',
+     'end_dt' : '9999-12-31',
+     'type': '공채',
+     'company_id': 0,
+     'company_name': '국민은행',
+     'status': '합격',
+   },
+   {
+    'apply_id': 3,
+    'post_id': 0,
+     'post_title': '2022년 상반기 공채 3',
+     'start_dt' : '0000-01-01',
+     'end_dt' : '9999-12-31',
+     'type': '공채',
+     'company_id': 0,
+     'company_name': '국민은행 3',
+     'status': '불합격',
+   },
+   {
+    'apply_id': 4,
+    'post_id': 0,
+     'post_title': '2022년 상반기 공채 4',
+     'start_dt' : '0000-01-01',
+     'end_dt' : '9999-12-31',
+     'type': '공채',
+     'company_id': 0,
+     'company_name': '국민은행 4',
+     'status': '지원 전',
+   },]
   const {goBack, push} = useInternalRouter();
+  const [applyCompanys,setApplyCompanys] = useState(dummyData);
+
+  const handleDeleteApplyCompany = function(event,apply_id){
+      // TODO High : Delete 요청 후 성공하면 삭제
+      if(confirm('정말 삭제하시겠습니까?')){
+        setApplyCompanys((prev)=>prev.filter(item=> item.apply_id !== apply_id))
+      }
+  }
   return (
     <div>
               <BothHeader left={<BackIcon onClick={()=>goBack()}/>}  right={<MyPageIcon onClick={()=>push('/myPage')}  />}  title="지원 현황 보기"></BothHeader>
               {/* //TODO middle : mounted 전에 해당 정보들에 대해 가져오기 */}
               {/* //TODO middle : 삭제버튼 클릭시, 페이지에서 먼저 삭제 후 axios로 삭제 요청 보낼지 고민 */}
-              {/* //TODO middle : CompanyList에 대해 데이터 연계 필요성 및 이벤트 적용 */}
               {/* //TODO low : 공고 클릭시 해당 회사 페이지로 이동 */}
               <div style = {{ margin: "3vh 5vw"
                 }}>
-                <ApplyCompanyList/>
+                <ApplyCompanyList applyCompanys={applyCompanys}  handleDeleteApplyCompany={handleDeleteApplyCompany}/>
               </div>
 
 
