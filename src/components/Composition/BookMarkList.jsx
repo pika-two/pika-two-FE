a7ff91340bc4da365ce3e6e37fccd1d464d3f793
pierrecw <leepierre456@gmail.com>
2022-08-14 07:16:57 +0900

    [ADD] 찜목록 삭제 이벤트 추가
    [ADD] 찜목록 삭제 이벤트 추가

--- src/components/Composition/BookMarkList.jsx
+++ src/components/Composition/BookMarkList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import TrashCanIcon from '../ui/icon/TrachCanIcon'
 import BookMarkListItem from '../ui/BookMarkListItem'
 import { useInternalRouter } from '../../pages/routing'
-export default function BookMarkList({bookmarks}) {
+export default function BookMarkList({bookmarks,handleDelete}) {
   const {push} = useInternalRouter();
   return (
         <div style={{
@@ -10,7 +10,7 @@ export default function BookMarkList({bookmarks}) {
       gridTemplateColumns : 'auto',
       gap : '1em'
     }}>
-        {bookmarks.map((bookmark)=><BookMarkListItem onClick={()=>push(`/company/${bookmark.company_id}`)} key={bookmark.fav_company_id} bookmark={bookmark} right={<TrashCanIcon/>}/>)}
+        {bookmarks.map((bookmark)=><BookMarkListItem onClick={()=>push(`/company/${bookmark.company_id}`)} key={bookmark.fav_company_id} bookmark={bookmark} right={<TrashCanIcon onClick={e=>handleDelete(e,bookmark.fav_company_id)}/>}/>)}
     </div>
   )
 }
--- src/pages/BookmarkPage.jsx
+++ src/pages/BookmarkPage.jsx
@@ -37,15 +37,22 @@ export default function BookmarkPage() {
     const {goBack, push} = useInternalRouter();
     const [bookmarks, setBookmarks] = useState(dummyData);
     // TODO : fetch로 데이터 가져오기
+
+
+    const handleDelete = function(event,fav_company_id){
+        if(confirm('정말 지우시겠습니까?')){
+          // TODO : 삭제 axios 보내기
+          setBookmarks(prev=>prev.filter(item=>item.fav_company_id !== fav_company_id))
+        }
+    }
   return (
     <div>
         <BothHeader left={<BackIcon onClick={()=>goBack()}/>}  right={<MyPageIcon onClick={()=>push('/myPage')}  />}  title="찜한 목록"></BothHeader>
         <div style = {{
           margin : "3vh 5vw"
         }}>
-          <BookMarkList bookmarks={bookmarks}/>
+          <BookMarkList bookmarks={bookmarks}  handleDelete={handleDelete}/>
         </div>
-    
     </div>
   )
 }
