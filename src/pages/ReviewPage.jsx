b28ddbba82cd1874b36d94eaa9a537ea1b3773f4
pierrecw <leepierre456@gmail.com>
2022-08-14 17:07:08 +0900

    [ADD] 리뷰 제출 이벤트 완료
    [ADD] 리뷰 제출 이벤트 완료

--- src/pages/ReviewPage.jsx
+++ src/pages/ReviewPage.jsx
@@ -4,11 +4,22 @@ import { useInternalRouter } from "./routing";
 import LeftOnlyHeader from '../components/Composition/LeftOnlyHeader';
 import FixedBottomButton from '../components/ui/FixedBottomButton'
 import Input from '../components/ui/Input';
-
+import {useRef}  from 'react'
 
 export default function ReviewPage() {
-
+   
    const {goBack,push} = useInternalRouter();
+   const reviewInputRef = useRef(null);
+   const submitReview = function(){
+    const value = reviewInputRef.current.value
+        if(value.trim().length){
+            // TODO axios 요청
+            if(confirm('리뷰 페이지로 가시겠습니까?')){
+                //TODO 회사 id로 연결하기 
+                push('/company/1')
+            }
+        }
+   }
    return(
     <div>
         {/* //TODO low : 리뷰 작성시 해당 기업의 리뷰 작성 api로 연결하기 */}
@@ -19,10 +30,10 @@ export default function ReviewPage() {
             marginTop: "30px",
         }}>
             {/* //TODO middle : input 이벤트 연계 */}
-            <Input height="50vh" 
+            <Input ref={reviewInputRef} height="50vh" 
             background = "#fffcab"/> 
         </div>
-        <FixedBottomButton onClick={()=>{push('/incomeConnect')}} background="#FFCC00">리뷰 제출하기</FixedBottomButton>
+        <FixedBottomButton onClick={submitReview} background="#FFCC00">리뷰 제출하기</FixedBottomButton>
     
     </div>
    )
