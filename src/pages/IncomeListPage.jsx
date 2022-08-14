dd7982f2ba440d2e46ce5168da34965097e1e138
pierrecw <leepierre456@gmail.com>
2022-08-14 19:48:14 +0900

    [DELETE] dummyData
    [DELETE] dummyData

--- src/pages/IncomeListPage.jsx
+++ src/pages/IncomeListPage.jsx
@@ -7,54 +7,8 @@ import IncomeList from '../components/Composition/IncomeList';
 import { useState } from 'react';
 import Message from '../components/ui/message';
 export default function IncomeListPage () {
-  const dummyData = [{
-      'date' : '2022-07-21',
-      'amount' : 1500000,
-      'memo' : '월정급여'
-    },
-    {
-      'date' : '2022-07-10',
-      'amount' : 300000,
-      'memo' : 'OOO 입금'
-    },
-    {
-      'date' : '2022-07-10',
-      'amount' : 100000,
-      'memo' : '보너스'
-    },
-    {
-      'date' : '2022-07-01',
-      'amount' : 20000,
-      'memo' : '중고거래'
-    },
-    {
-      'date' : '2022-06-21',
-      'amount' : 1500000,
-      'memo' : '월정급여'
-    },
-    {
-      'date' : '2022-06-16',
-      'amount' : 20000,
-      'memo' : '환급'
-    },
-    {
-      'date' : '2022-06-17',
-      'amount' : 20000,
-      'memo' : 'OOO 입금'
-    },
-    {
-      'date' : '2022-07-01',
-      'amount' : 20000,
-      'memo' : 'OOO 입금'
-    },
-    {
-      'date' : '2022-06-11',
-      'amount' : 1500000,
-      'memo' : '보너스'
-    },
-]
    const {goBack,push} = useInternalRouter();
-   const [incomes, setIncomes]  = useState(dummyData);
+   const [incomes, setIncomes]  = useState([]);
    const [selectedIncomeNameList, setSelectedIncomeNameLIst] = useState([]);
    const handleChange = function(event,name){
         setSelectedIncomeNameLIst(prev => prev.indexOf(name) === -1?[...prev,name]:[...prev].filter(item=>item !== name))
