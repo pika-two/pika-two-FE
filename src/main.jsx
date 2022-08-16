623d0b0b24a12effd2d3392c5e6295605b5e0a8e
pierrecw <leepierre456@gmail.com>
2022-08-16 18:24:46 +0900

    [ADD] Recoil 추가
    [ADD] Recoil 추가

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import {BrowserRouter} from 'react-router-dom'
 import { worker } from './mocks/browser'
-
+import { RecoilRoot } from "recoil";
 // if (process.env.NODE_ENV === 'development') {
 //   worker.start()
 // }
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </RecoilRoot>
   </React.StrictMode>
 )
--- src/shared/store.js
+++ src/shared/store.js
@@ -1,20 +1,20 @@
 import { atom,selector } from "recoil";
 
 
-export const userInfoStore = atom({
-    key: 'userInfo',
-    default: {},
-    effects : [localStorageEffect('pika-two')]
-  });
 const localStorageEffect = (key) => ({setSelf,onSet}) => {
     const savedValue = localStorage.getItem(key)
     if(savedValue !== null){
         setSelf(JSON.parse(savedValue));
     }
-    onSet((newValue,isReset)=>{
+    onSet((newValue,_,isReset)=>{
         isReset?localStorage.removeItem(key):localStorage.setItem(key,JSON.stringify(newValue))
     })
 }
+export const userInfoStore = atom({
+    key: 'userInfo',
+    default: {},
+    effects : [localStorageEffect('pikaTwo')]
+  });
 export const userisLogined = selector({
     key : 'isLogined',
     get : ({get})=> {
