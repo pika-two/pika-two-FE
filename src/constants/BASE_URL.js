93772fc4961480cba150881acf0e7084cd393f75
pierrecw <leepierre456@gmail.com>
2022-08-17 16:48:15 +0900

    [FIX] 수정
    [FIX] 수정

--- src/constants/BASE_URL.js
+++ src/constants/BASE_URL.js
@@ -1,8 +1,3 @@
-
-const mode = import.meta.env.MODE;
 let baseURL = "http://pikatwo.kbfg.kubepia.com/";
-if (mode !== "dev") {
-  baseURL = "https://test.api.predev.dev/";
-}
 
 export default baseURL
