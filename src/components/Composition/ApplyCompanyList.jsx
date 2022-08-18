5c47f251997aaba5dc37512825bcace7b345742a
pierrecw <leepierre456@gmail.com>
2022-08-18 14:59:03 +0900

    [ADD] Apply 삭제 추가
    [ADD] Apply 삭제 추가

--- src/components/Composition/ApplyCompanyList.jsx
+++ src/components/Composition/ApplyCompanyList.jsx
@@ -1,7 +1,7 @@
 import TrashCanIcon from '../ui/icon/TrashCanIcon'
 import ApplyCompanyListItem from '../ui/ApplyCompanyListItem'
 import {useInternalRouter} from '../../pages/routing';
-export default function ApplyCompanyList({applyCompanys, handleDeleteApplyCompany}) {
+export default function ApplyCompanyList({applyCompanys, handleOpen}) {
   const {push} = useInternalRouter();
   return (
     <div style={{
@@ -9,7 +9,7 @@ export default function ApplyCompanyList({applyCompanys, handleDeleteApplyCompan
         gridTemplateColumns : 'auto',
         gap : '1em'
       }}>
-          {applyCompanys?.map((applyCompany)=><ApplyCompanyListItem key={applyCompany.apply_id}  onClick={()=> {push(`/company/${applyCompany.company_id}`)}}  applyCompanpy={applyCompany} right={<TrashCanIcon onClick={(e)=>handleDeleteApplyCompany(e,applyCompany.apply_id)}/>}/>)}
+          {applyCompanys?.map((applyCompany)=><ApplyCompanyListItem key={applyCompany.apply_id}  onClick={()=> {push(`/company/${applyCompany.company_id}`)}}  applyCompanpy={applyCompany} right={<TrashCanIcon onClick={(e)=>handleOpen(e,applyCompany.apply_id)}/>}/>)}
       </div>
   )
 }
