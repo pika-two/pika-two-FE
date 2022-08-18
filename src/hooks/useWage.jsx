8f5e59d41c8757e24045350bc83e83cfdde71a94
pierrecw <leepierre456@gmail.com>
2022-08-18 16:42:54 +0900

    [ADD] 정렬
    [ADD] 정렬

--- src/hooks/useWage.jsx
+++ src/hooks/useWage.jsx
@@ -3,7 +3,7 @@ import companyService from "../apis/company";
 
 export default function useWage(company_id,year){
     const {data, error} = useSWR(['api/wage/year',company_id,year],()=>companyService.getCompanyWage(company_id,year))
-
+    data?.wages.sort((a,b)=>b.wage-a.wage)
     return  {
         companyWageInfo : data,
         isLoading: !error && data === undefined,
