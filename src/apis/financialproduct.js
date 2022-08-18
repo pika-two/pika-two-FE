8b2cc3750fdb2b9bef8b2171585ba52409546d20
pierrecw <leepierre456@gmail.com>
2022-08-19 00:45:55 +0900

    [ADD] await 추가
    [ADD] await 추가

--- src/apis/financialproduct.js
+++ src/apis/financialproduct.js
@@ -1,7 +1,9 @@
 import baseURL from "../constants/BASE_URL";
+import timeout from "../utils/timeout";
 import customAxios from "./customAxios";
 const financialproductService = {
     async getFinance(user_id){
+        await timeout(2000)
         const {data,status} = await customAxios.get(baseURL+`api/financialproduct/${user_id}`)
         const {data : responseData} = data;
         return responseData
